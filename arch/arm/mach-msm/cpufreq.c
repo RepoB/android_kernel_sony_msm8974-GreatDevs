72d3b88e90c6641b98846d02e6f5bf1f5bd96dae
Paul Reioux <reioux@gmail.com>
2013-11-02 19:02:47 -0500

    mach-msm: cpufreq: remove deprecated early_suspend drivers
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -17,7 +17,6 @@
  *
  */
 
-#include <linux/earlysuspend.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/cpufreq.h>
@@ -406,10 +405,6 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 
 	return 0;
 }
-
-#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
-extern bool lmf_screen_state;
-#endif
 
 static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 		unsigned long action, void *hcpu)
